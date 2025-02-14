68349965fc99dfacb430601ca486922522b008c9
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-02-14 10:46:14 +0100

    added prfile to auth middleware

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -5,7 +5,7 @@ import { useSupabaseUser } from '#imports'
 export default defineNuxtRouteMiddleware((to, from) => {
   const user = useSupabaseUser()
   // If user is not logged in, redirect to login for protected routes
-  if (to.path.startsWith('/dashboard') && !user.value) {
+  if (to.path.startsWith('/dashboard') || to.path.startsWith('/profile') && !user.value) {
     return navigateTo('/login')
   }
 })
