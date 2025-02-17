c0d5f04a69fe9c67473795717d5340117ce6c643
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-02-17 10:17:24 +0100

    centered link

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -1,25 +1,29 @@
 <template>
   <nav class="bg-white shadow">
-    <div class="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
-      <div class="text-xl font-bold">
+    <div
+      class="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between font-semibold"
+    >
+      <div class="text-xl">
         <NuxtLink to="/">Verity.ai</NuxtLink>
       </div>
-      <div v-if="user">
+      <div v-if="user" class="flex gap-x-4 items-center">
         <NuxtLink to="/dashboard">Dashboard</NuxtLink>
         <Button variant="secondary" @click="logout">Log Out</Button>
       </div>
       <div v-else>
-        <NuxtLink to="/login" class="text-purple-600 font-bold">Log In</NuxtLink>
+        <NuxtLink to="/login" class="text-purple-600 font-bold"
+          >Log In</NuxtLink
+        >
       </div>
     </div>
   </nav>
 </template>
 <script setup lang="ts">
 const user = useSupabaseUser()
-const supabase = useSupabaseClient();
+const supabase = useSupabaseClient()
 
 async function logout() {
-  await supabase.auth.signOut();
-  navigateTo("/login");
+  await supabase.auth.signOut()
+  navigateTo('/login')
 }
 </script>
