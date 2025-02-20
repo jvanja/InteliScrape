d89b5364b61a1703fae308d142524f2a18fb4797
Vanja Jelic <vanjajelic@Vanjas-Mac-mini.local>
2025-02-20 12:32:24 +0100

    yay

--- pages/payment-success.vue
+++ pages/payment-success.vue
@@ -13,17 +13,20 @@ const { customer, accountType, subscriptionExpires, error } = await $fetch<{
 <template>
   <div class="prose lg:prose-xl m-5">
     <p v-if="error">Error finalizing subscription: {{ error }}</p>
-    <p v-else-if="customer && !customer.deleted">
-      Thank you, {{ customer.name || customer.email }}! Your subscription is now
-      active.
+    <div v-else-if="customer && !customer.deleted">
+      <h1 class="text-xl text-green-700">
+        Thank you, <strong>{{ customer.name || customer.email }}</strong>! Your subscription is
+        now active.
+      </h1>
       <br />
-      Your account type is: {{ accountType }}
+      Your account type is: <strong>{{ accountType }}</strong>
       <br />
-      Your subscription will expire on: {{ subscriptionExpires }}
-    </p>
+      Your subscription will expire on:
+      <strong>{{ formatDate(subscriptionExpires!) }}</strong>
+    </div>
     <p v-else-if="customer && customer.deleted">
       It appears your Stripe customer information has been deleted!
     </p>
-    <p>Go to Your <NuxtLink to="/dashboard">Dashboard</NuxtLink></p>
+    <p class="mt-4">Go to Your <NuxtLink to="/dashboard"><Button variant="outline">Dashboard</Button></NuxtLink></p>
   </div>
 </template>
